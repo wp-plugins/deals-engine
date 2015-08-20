e46505bdc5fd43ff6f02e4f5a274d90feb6d7c86
wpsocial <wpsocial@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-20 07:50:51 +0000

    update 2.2.3
    git-svn-id: https://plugins.svn.wordpress.org/deals-engine/tags/2.2.3@1225941 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/templates/single-deal/image.php
@@ -0,0 +1,21 @@
+<?php 
+
+/**
+ * Image Template
+ * 
+ * Override this template by copying it to <ourtheme/deals-engine/single-deal/image.php
+ * 
+ * @author 		Social Deals Engine
+ * @package 	Deals-Engine/Includes/Templates
+ * @version     1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+?>
+
+<div class="deals-single-image">
+
+	<?php echo $dealimg; ?>
+	
+</div>
