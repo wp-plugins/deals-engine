f2521137658ed627445f4c85c100a4b02f434f76
wpsocial <wpsocial@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-16 11:56:10 +0000

    release 2.2.4
    git-svn-id: https://plugins.svn.wordpress.org/deals-engine/tags/2.2.4@1246601 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/templates/home-deals/header/image.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Image template for the top placed Deal on the Deals home page.
+ * 
+ * Override this template by copying it to yourtheme/deals-engine/home-deals/header/image.php
+ * 
+ * @author 		Social Deals Engine
+ * @package 	Deals-Engine/Includes/Templates
+ * @version     1.0.0
+ */
+ 
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+?>	
+		
+<div class="deals-img">
+
+	<div class="deals-img-flag-big" itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+	
+		<span itemprop="price"><?php echo $price; ?></span>
+		<link itemprop="availability" href="http://schema.org/InStock" />
+		
+	</div>
+
+	<a href="<?php echo $dealurl; ?>" title="<?php echo $dealtitle; ?>">
+		<img src="<?php echo $dealimgurl; ?>" alt="<?php echo $dealtitle; ?>" title="<?php echo $dealtitle; ?>" width="100%">
+	</a>
+		
+</div>
